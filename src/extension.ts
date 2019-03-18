f6c6adc458d8fc8e0f5228c8f4b51c5495b4c6cd
Richard Edwards <richardgedwards@gmail.com>
2019-03-17 19:51:51 -0600

    added keybindings

--- src/extension.ts
+++ src/extension.ts
@@ -9,32 +9,59 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 		console.log('Congratulations, your extension "ispy" is now active!');
+	// let terminal = vscode.window.activeTerminal;
+	// if(terminal === undefined) {
+	// 	terminal = vscode.window.createTerminal();
+	// }
+	// terminal.sendText('ipython --pylab')	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let run = vscode.commands.registerCommand('ispy.run', () => {
-		const terminal = vscode.window.activeTerminal;
+	let slected = vscode.commands.registerCommand('ispy.selected', () => {
+		let terminal = vscode.window.activeTerminal;
 		const editor = vscode.window.activeTextEditor;
 		if (editor !== undefined) {
-			const filename = editor.document.fileName;
-			if(terminal !== undefined && editor !== undefined) {
+			if(terminal !== undefined) {
+				terminal.show();
 				if(editor.document.isDirty) {
 					editor.document.save();
 				}	
-				terminal.sendText(`%run -i ${filename}`);
+				terminal.sendText(`%run -i ${editor.document.fileName}`);
 			} else {
-				vscode.window.showErrorMessage('No terminal available');
+				vscode.window.showWarningMessage('No terminal available');
 			}
 		} else {
-			vscode.window.showErrorMessage('No editor available');
+			vscode.window.showWarningMessage('No editor available');
 		}
 	});
-	let start = vscode.commands.registerCommand('ispy.start', () => {
-		const terminal = vscode.window.activeTerminal;
-		if(terminal !== undefined) {
-			terminal.sendText('ipython --pylab');
+
+
+	let run = vscode.commands.registerCommand('ispy.run', () => {
+		let terminal = vscode.window.activeTerminal;
+		const editor = vscode.window.activeTextEditor;
+		if (editor !== undefined) {
+			if(terminal !== undefined) {
+				terminal.show()
+				if(editor.document.isDirty) {
+					editor.document.save();
+				}	
+				terminal.sendText(`%run -i ${editor.document.fileName}`);
+			} else {
+				vscode.window.showWarningMessage('No terminal available');
+			}
+		} else {
+			vscode.window.showWarningMessage('No editor available');
 		}
+	});
+
+	let start = vscode.commands.registerCommand('ispy.start', () => {
+		let terminal = vscode.window.activeTerminal;
+		terminal = vscode.window.activeTerminal;
+		if(terminal === undefined) {
+			terminal = vscode.window.createTerminal();
+		}
+		terminal.sendText('ipython --pylab');	
 	});
 
 	context.subscriptions.push(run);
